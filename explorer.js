57c393041ed1d6cf9fc18a26522e125e6e598d94
Eugene Ilagin <eugene.ilagin@gmail.com>
2018-03-28 16:03:00 +0300

    Add airbnb linter to explorer code #959

--- explorer.js
+++ explorer.js
@@ -1,32 +1,31 @@
-/*jslint node: true */
-"use strict";
+/* jslint node: true */
+
+'use strict';
+
 require('./relay');
-var conf = require('core/conf.js');
-var eventBus = require('core/event_bus.js');
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var ws = require('./controllers/ws');
+const conf = require('core/conf.js');
+const eventBus = require('core/event_bus.js');
+const express = require('express');
 
-app.use(express.static(__dirname + '/public'));
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const ws = require('./controllers/ws');
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/views/index.html');
-});
+app.use(express.static(`${__dirname}/public`));
 
-eventBus.on('new_joint', function() {
-	io.sockets.emit('update');
-});
+app.get('/', (req, res) => { res.sendFile(`${__dirname}/views/index.html`); });
 
-io.on('connection', function(socket) {
-	socket.on('start', ws.start);
-	socket.on('next', ws.next);
-	socket.on('prev', ws.prev);
-	socket.on('new', ws.newUnits);
-	socket.on('info', ws.info);
-	socket.on('highlightNode', ws.highlightNode);
-	socket.on('nextPageTransactions', ws.nextPageTransactions);
+eventBus.on('new_joint', () => { io.sockets.emit('update'); });
+
+io.on('connection', (socket) => {
+    socket.on('start', ws.start);
+    socket.on('next', ws.next);
+    socket.on('prev', ws.prev);
+    socket.on('new', ws.newUnits);
+    socket.on('info', ws.info);
+    socket.on('highlightNode', ws.highlightNode);
+    socket.on('nextPageTransactions', ws.nextPageTransactions);
 });
 
 server.listen(conf.webPort);
--- relay.js
+++ relay.js
@@ -1,30 +1,34 @@
-/*jslint node: true */
-"use strict";
-var conf = require('core/conf.js');
-var myWitnesses = require('core/my_witnesses.js');
+/* jslint node: true */
+
+'use strict';
+
+const conf = require('core/conf.js');
+const myWitnesses = require('core/my_witnesses.js');
 
 
-function replaceConsoleLog () {
-	var clog = console.log;
-	console.log = function () {
-		Array.prototype.unshift.call(arguments, Date().toString() + ':');
-		clog.apply(null, arguments);
-	}
+function replaceConsoleLog() {
+    const clog = console.log;
+    console.log = function (...arguments) {
+        Array.prototype.unshift.call(arguments, `${Date().toString()}:`);
+        clog(...arguments);
+	};
 }
 
-function start () {
-	console.log('starting');
-	var network = require('core/network.js');
-	if (conf.initial_peers)
-		conf.initial_peers.forEach(function (url) {
-			network.findOutboundPeerOrConnect(url);
-		});
+function start() {
+    console.log('starting');
+    const network = require('core/network.js');
+    if (conf.initial_peers) {
+        conf.initial_peers.forEach((url) => {
+        network.findOutboundPeerOrConnect(url);
+    });
+}
 }
 
 replaceConsoleLog();
-myWitnesses.readMyWitnesses(function (arrWitnesses) {
-	if (arrWitnesses.length > 0)
-		return start();
-	console.log('will init witnesses', conf.initial_witnesses);
-	myWitnesses.insertWitnesses(conf.initial_witnesses, start);
+myWitnesses.readMyWitnesses((arrWitnesses) => {
+    if (arrWitnesses.length > 0) {
+        return start();
+    }
+    console.log('will init witnesses', conf.initial_witnesses);
+    myWitnesses.insertWitnesses(conf.initial_witnesses, start);
 }, 'ignore');
