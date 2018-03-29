35b617379bb2536fe996aae48c4ff9de1e6bf7a4
dkostenevich <dima.spaker@gmail.com>
2018-03-29 12:41:22 +0300

    Update to latest byteball-explorer: #4d9d5d5 #b3208e9 #98d5e43 #ea72545 #0953e03

--- controllers/address.js
+++ controllers/address.js
@@ -7,15 +7,15 @@ var async = require('async');
 var BIGINT = 9223372036854775807;
 
 function getAmountForInfoAddress(objTransactions, cb) {
-	var arrTransactionsUnit = [], key;
+	var arrTransactionsUnits = [], key;
 	for (key in objTransactions) {
-		if (arrTransactionsUnit.indexOf(objTransactions[key].unit) == -1) arrTransactionsUnit.push(objTransactions[key].unit);
+		if (arrTransactionsUnits.indexOf(objTransactions[key].unit) == -1) arrTransactionsUnits.push(objTransactions[key].unit);
 	}
 	db.query("SELECT inputs.unit, outputs.address, outputs.amount, outputs.asset FROM inputs, outputs \n\
 		WHERE inputs.unit IN (?) AND outputs.unit = inputs.src_unit AND outputs.message_index = inputs.src_message_index AND outputs.output_index = inputs.src_output_index",
-		[arrTransactionsUnit], function(rowsAmount) {
+		[arrTransactionsUnits], function(rowsAmount) {
 			db.query("SELECT unit, asset, type, serial_number, from_main_chain_index, to_main_chain_index, amount, address \n\
-				FROM inputs WHERE unit IN (?) AND (type='issue' OR type='headers_commission' OR type='witnessing')", [arrTransactionsUnit], function(rows) {
+				FROM inputs WHERE unit IN (?) AND (type='issue' OR type='headers_commission' OR type='witnessing')", [arrTransactionsUnits], function(rows) {
 				rowsAmount.forEach(function(row) {
 					key = row.unit + '_' + row.asset;
 					if (objTransactions[key]) objTransactions[key].from.push({
@@ -68,19 +68,19 @@ function getAmountForInfoAddress(objTransactions, cb) {
 }
 
 function getSpentOutputs(objTransactions, cb) {
-	var arrTransactionsUnit = [], key, key2;
+	var arrTransactionsUnits = [], key, key2;
 	for (key in objTransactions) {
-		if (arrTransactionsUnit.indexOf(objTransactions[key].unit) == -1) arrTransactionsUnit.push(objTransactions[key].unit);
+		if (arrTransactionsUnits.indexOf(objTransactions[key].unit) == -1) arrTransactionsUnits.push(objTransactions[key].unit);
 	}
-	var n = 0, l = arrTransactionsUnit.length - 1;
+	var n = 0, l = arrTransactionsUnits.length - 1;
 
 	function setSpentOutputs() {
 		db.query("SELECT outputs.output_id, outputs.message_index, outputs.output_index, outputs.asset, inputs.unit \n \
 		FROM outputs, inputs WHERE outputs.unit = ? AND is_spent = 1 AND inputs.src_unit = outputs.unit \n\
 		AND inputs.src_message_index = outputs.message_index AND inputs.src_output_index = outputs.output_index",
-			[arrTransactionsUnit[n]], function(rows) {
+			[arrTransactionsUnits[n]], function(rows) {
 				rows.forEach(function(row) {
-					key = arrTransactionsUnit[n] + '_' + row.asset;
+					key = arrTransactionsUnits[n] + '_' + row.asset;
 					key2 = row.output_id + '_' + row.message_index + '_' + row.output_index;
 					if (objTransactions[key] && objTransactions[key].to[key2]) {
 						objTransactions[key].to[key2].spent = row.unit;
@@ -102,8 +102,8 @@ function getSpentOutputs(objTransactions, cb) {
 function getUnitsForTransactionsAddress(address, lastInputsROWID, lastOutputsROWID, cb) {
 	db.query("SELECT inputs.unit, MIN(inputs.ROWID) AS inputsROWID, MIN(outputs.ROWID) AS outputsROWID \n\
 		FROM inputs, outputs, units \n\
-		WHERE (( inputs.unit IN (SELECT unit FROM inputs WHERE address = ? AND ROWID < ? GROUP BY unit ORDER BY ROWID DESC LIMIT 0, 5)) \n\
-		OR ( outputs.unit IN (SELECT unit FROM outputs WHERE address = ? AND ROWID < ? GROUP BY unit ORDER BY ROWID DESC LIMIT 0, 5))) \n\
+		WHERE (( units.unit IN (SELECT DISTINCT unit FROM inputs WHERE address = ? AND ROWID < ? ORDER BY ROWID DESC LIMIT 0, 5)) \n\
+		OR ( units.unit IN (SELECT DISTINCT unit FROM outputs WHERE address = ? AND ROWID < ? ORDER BY ROWID DESC LIMIT 0, 5))) \n\
 		AND inputs.unit = outputs.unit AND (( inputs.asset IS NULL AND outputs.asset IS NULL ) OR (inputs.asset = outputs.asset)) \n\
 		AND units.unit = inputs.unit \n\
 		GROUP BY inputs.unit \n\
@@ -116,15 +116,15 @@ function getUnitsForTransactionsAddress(address, lastInputsROWID, lastOutputsROW
 }
 
 function getAddressTransactions(address, lastInputsROWID, lastOutputsROWID, cb) {
-	getUnitsForTransactionsAddress(address, lastInputsROWID, lastOutputsROWID, function(arrUnit, newLastInputsROWID, newLastOutputsROWID) {
-		if (arrUnit.length) {
+	getUnitsForTransactionsAddress(address, lastInputsROWID, lastOutputsROWID, function(arrUnits, newLastInputsROWID, newLastOutputsROWID) {
+		if (arrUnits.length) {
 			db.query("SELECT inputs.unit, units.creation_date, inputs.address, outputs.address AS addressTo, outputs.amount, inputs.asset, outputs.asset AS assetTo, outputs.output_id, outputs.message_index, outputs.output_index, inputs.type, "+ db.getUnixTimestamp("units.creation_date")+" AS timestamp \n\
 		FROM inputs, outputs, units \n\
-		WHERE (( inputs.unit IN (?) AND outputs.unit = inputs.unit ) OR ( outputs.unit IN (?) AND inputs.unit = outputs.unit )) \n\
+		WHERE units.unit IN (?) AND outputs.unit = inputs.unit \n\
 		AND (( inputs.asset IS NULL AND outputs.asset IS NULL ) OR (inputs.asset = outputs.asset)) \n\
 		AND units.unit = inputs.unit \n\
 		ORDER BY units.main_chain_index DESC",
-				[arrUnit, arrUnit], function(rowsTransactions) {
+                [arrUnits], function(rowsTransactions) {
 					var key, objTransactions = {};
 					if (rowsTransactions.length) {
 						rowsTransactions.forEach(function(row) {
